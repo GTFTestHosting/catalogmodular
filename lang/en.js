d1f78cbb867781220c6c8dda212b3d511f31a9d7
GTFTestHosting <chris@gracianatortilla.com>
2025-10-03 11:15:13 -0700

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,10 +1,10 @@
 {
-    "companyTitle": "Your Company",
-    "companySubtitle": "High-Quality Products & Solutions",
+    "companyTitle": "Graciana Tortilla Factory",
+    "companySubtitle": "Product Catalog 2025",
     "modalSpecs": "Specifications",
     "modalShipping": "Shipping Information",
-    "footerRights": "© 2025 Your Company Name. All Rights Reserved.",
-    "footerContact": "Contact us:",
+    "footerRights": "© Graciana Tortilla Factory. All Rights Reserved.",
+    "footerContact": "Contact us: support@gracianatortilla.com",
     "backButton": "Back",
     "loading": "Loading..."
 }
