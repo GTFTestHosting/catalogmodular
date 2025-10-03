5998568ab09a9bbb7f93435219957d56393d56f6
GTFTestHosting <chris@gracianatortilla.com>
2025-10-03 11:14:04 -0700

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,10 @@
+{
+    "companyTitle": "Your Company",
+    "companySubtitle": "High-Quality Products & Solutions",
+    "modalSpecs": "Specifications",
+    "modalShipping": "Shipping Information",
+    "footerRights": "© 2025 Your Company Name. All Rights Reserved.",
+    "footerContact": "Contact us:",
+    "backButton": "Back",
+    "loading": "Loading..."
+}
